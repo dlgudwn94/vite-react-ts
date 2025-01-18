7adccc96fa78b44cc446fe68f322eb00f81cf980
dlgudwn94 <dlgudwn82@naver.com>
2025-01-19 01:33:00 +0900

    [구성] React 플러그인 구성

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
-  server: {
-    host: "localhost",
-    port: 3000,
-  },
-  preview: {
-    port: 8080,
-  },
+  plugins: [
+    react({
+      jsxRuntime: "automatic",
+    }),
+  ],
+  // ...
 });
