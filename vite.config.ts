92df03aa6697ca53e2bca9a01bc90945e2b40ac5
dlgudwn94 <dlgudwn82@naver.com>
2025-01-19 04:38:40 +0900

    [구성] TypeScript 구성

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-/** @type {import('vite').UserConfig} */
 export default defineConfig({
   plugins: [
     react({
       jsxRuntime: "automatic",
     }),
   ],
-  // ...
+  server: {
+    host: "localhost",
+    port: 3000,
+  },
+  preview: {
+    host: "localhost",
+    port: 8080,
+  },
 });
